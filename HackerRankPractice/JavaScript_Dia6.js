79636f58684838117f999ad86af7fe53a398e746
cchavezmx <cchavezmx@outlook.com>
2020-08-03 00:59:58 -0500

    Hacker y Katas

--- HackerRankPractice/JavaScript_Dia6.js
+++ HackerRankPractice/JavaScript_Dia6.js
@@ -18,4 +18,22 @@ let prueba = input.split(' ').slice(1)   //TOMAMOS EL STRING Y LO TRANSFORMAMOS 
 //EN EL SITIO USA \n para separar los espacios. 
 let word = prueba.forEach((value) => {buldierWord(value)}) // CREAMOS LA VARIABLE QUE CONTENDRA EL RESULTADO DESEADO, CON UN FOREACH DE CADA UNO DE LOS ELEMENTOS LE APLICAREMOS LA FUNCION QUE SEPARARA LAS LETRAS
  
-console.log(prueba)
+// console.log(prueba)
+
+
+
+let paises = ["México", "Panamá", "Guatemala", "El Salvador"]
+
+function numeros(string){return Number(string.length)}  // crea los numeros 
+
+function longestCountry(array){
+
+    let test2 = new Array(1).fill(array.map(value => numeros(value))) // se crea un arreglo con esos numeros
+    let compara = test2[0].sort((a,b) => b - a) // acomodamos del mayor al menor
+    let final = array.filter(value => value.length === compara[0]) // obtenemos el nombre. 
+
+    return final.join('') // pasamos el arreglo a un string 
+
+}
+
+console.log(longestCountry(paises))
